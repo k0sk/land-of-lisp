cd0a0b5ac351b56d8f06ab6c1d610e3e1721e2b6
kosk <mail@kosk.me>
2015-12-30 11:46:36 +0900

    Add example programs from chapter 1 to 5

---
+++ guess-my-number.lisp
@@ -0,0 +1,27 @@
+(defparameter *small* 0)
+(defparameter *big* 100)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number)
+  )
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number)
+  )
+
+(defun start-over ()
+  (defparameter *small* 0)
+  (defparameter *big* 100)
+  (guess-my-number)
+  )
+
+(guess-my-number)
+(bigger)
+(smaller)
+(start-over)
+
